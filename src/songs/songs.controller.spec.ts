25faabcc76c5361ae2eb2d78fac3ac1ca6fad888
Taswoor Hussain <taswoorhossein72@gmail.com>
2024-07-16 13:08:42 +0500

    add create song controller and also set global pipe for validation....

--- src/songs/songs.controller.spec.ts
+++ src/songs/songs.controller.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SongsController } from './songs.controller';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { SongsController } from './songs.controller';
 
-describe('SongsController', () => {
-  let controller: SongsController;
+// describe('SongsController', () => {
+//   let controller: SongsController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [SongsController],
-    }).compile();
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [SongsController],
+//     }).compile();
 
-    controller = module.get<SongsController>(SongsController);
-  });
+//     controller = module.get<SongsController>(SongsController);
+//   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+//   it('should be defined', () => {
+//     expect(controller).toBeDefined();
+//   });
+// });
--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,21 @@
+import { Post , Body, HttpException } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { SongsService } from './songs.service';
+import { createsongDTO } from './dto/createsongs.dto';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+    constructor(private readonly songservice:SongsService){}
+
+@Post()
+ create(@Body() song:createsongDTO){
+    try {
+         this.songservice.create(song)
+         return song
+    } catch (error) {
+        throw new HttpException(error,401)
+    }
+ }
+
+
+}
